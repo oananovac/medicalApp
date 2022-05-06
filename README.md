9f5f95a17c72a3f60ec75c826f7d567614ddf5d2
Cristina <cristinamaria.miulescu@gmail.com>
2022-05-06 14:32:05 +0300

    Connect server to MongoDB Atlas

--- README.md
+++ README.md
@@ -5,12 +5,42 @@
 ```bash
 mkdir medicalApp
 cd medicalApp
+
 npm init --yes
 npm i express dotenv
+
+npm i --save-dev nodemon concurrently
 ```
 
-To start the app:
+To start the server:
 
 ```bash
-npm start
+npm run server
+```
+
+## Step 2: Connect server to MongoDB Atlas
+
+## 2.1 Create a new database on MongoDB Atlas
+
+https://www.mongodb.com/docs/atlas/getting-started/
+
+## 2.2 Connect to MongoDB Atlas
+
+```bash
+npm i mongoose
+```
+
+```javascript
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("[*] Connected to database");
+
+    app.listen(process.env.PORT, (req, res) => {
+      console.log("[*] Server started on port: " + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("[*] Error: " + err);
+  });
 ```
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,6 +11,19 @@ app.get("/", (req, res) => {
   res.send("React App");
 });
 
-app.listen(process.env.PORT, (req, res) => {
-  console.log("[*] Server started on port: " + process.env.PORT);
-});
+/**
+ * Connects to MongoDB Atlas
+ * Then starts the server
+ */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("[*] Connected to database");
+
+    app.listen(process.env.PORT, (req, res) => {
+      console.log("[*] Server started on port: " + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("[*] Error: " + err);
+  });
